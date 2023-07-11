d1e49c4df2a0eaada29f1a06d1164d17928ba7a0
Jerson Rodriguez <jersonrn@gmail.com>
2023-07-10 23:59:59 -0500

    Added basic ShowFloatDefinition command

--- rplugin/python3/code_insight.py
+++ rplugin/python3/code_insight.py
@@ -1,7 +1,7 @@
 import pynvim
 import pynvim.api
 
-default_settings = {'focusable': True,
+default_config = {'focusable': True,
                     'external': False,
                     'width': 70,
                     'height': 20,
@@ -27,13 +27,30 @@ def fix_config(config: dict) -> dict:
 class CodeInsight(object):
     def __init__(self, nvim) -> None:
         self.nvim = nvim
+        self.config: dict
+        self.windows: dict = {}
 
     @pynvim.autocmd('VimEnter', sync=True)
     def vim_enter(self) -> None:
         try: config = self.nvim.eval('g:code_insight_config')
-        except: config = default_settings
+        except: config = default_config
         self.config = config if type(config['focusable']) == bool else fix_config(config)
 
-    @pynvim.command('ShowConfig', nargs='*') # type: ignore
-    def show_config(self, args) -> None:
-        self.nvim.command(f'echo "{self.config}"')
+    @pynvim.command('ShowFloatDefinition') # type: ignore
+    def show_float_definition(self) -> None:
+        definitions = self.nvim.call('coc#rpc#request', 'definitions', [])
+        if definitions:
+            current_def = 0
+            uri = definitions[current_def]['uri']
+            buffer = self.nvim.exec_lua('return vim.uri_to_bufnr(...)', uri)
+            win_id = self.nvim.call('nvim_open_win', buffer, 1, self.config)
+
+            self.windows[win_id] = {'current_def': current_def, 'definitions': definitions}
+            # win_info = self.nvim.call('getwininfo')
+            self.nvim.command(f'echo "{self.windows}"')
+            # self.nvim.api.win_close(W,True)
+
+
+    @pynvim.command('Print', nargs='*') # type: ignore
+    def __print(self, args) -> None:
+        self.nvim.out_write(f'{args}\n')
